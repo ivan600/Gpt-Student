713349774624bd5dfa68c1e4f248483762461c43
ivan600 <57300867+ivan600@users.noreply.github.com>
2023-06-14 23:36:18 +0200

    0.7

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -23,93 +23,123 @@ namespace WpfApp1
 /// </summary>
 public partial class MainWindow : Window
 {
-        OpenAIAPI api = new OpenAIAPI("YOUR_APIKEY");
+        OpenAIAPI api;
 
 
-        public MainWindow()
-        {
-            InitializeComponent();
-            this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
-            Grid1.PreviewKeyUp += TxtMensaje_KeyDown;
-            chat = api.Chat.CreateConversation();
+public MainWindow()
+{
+InitializeComponent();
+this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+Grid1.PreviewKeyUp += TxtMensaje_KeyDown;
 
-        }
 
-        string response;
-        private void TxtMensaje_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.Key == Key.Enter)
+}
+
+string response;
+private void TxtMensaje_KeyDown(object sender, KeyEventArgs e)
+{
+if (e.Key == Key.Enter)
+{
+GenerarRespuesta();
+}
+else if (e.Key == Key.Escape) { Close(); }
+}
+
+Conversation chat;
+string role;
+private void GenerarRespuesta()
+{
+
+            try
             {
-                GenerarRespuesta();
+                if(api != null)
+                {
+                    chat.AppendUserInput(Prompt.Text);
+                    AgregarPrompt(Prompt.Text);
+                    AgregarRespuesta();
+
+                    Prompt.Text = string.Empty;
+                }else
+                {
+                    MessageBox.Show($"Se ha producido un error: No has introducido la apikey", "Error");
+                }
+                
             }
-            else if (e.Key == Key.Escape) { Close(); }
-        }
-
-        Conversation chat;
-        string role;
-        private void GenerarRespuesta()
-        {
+            catch(Exception ex) { 
             
-            
-            chat.AppendUserInput(Prompt.Text);
-            AgregarPrompt(Prompt.Text);
-            AgregarRespuesta();
+                MessageBox.Show($"Se ha producido un error: {ex.Message}");
+            }
 
-            Prompt.Text = string.Empty;
-        }
+}
 
-        Thickness margin_TxtBlk = new Thickness(40, 5, 40, 5);
-        double fontSize = 15;
-        private async void AgregarRespuesta()
-        {
-            string respuesta = string.Empty;
-            response = string.Empty;
+Thickness margin_TxtBlk = new Thickness(40, 5, 40, 5);
+double fontSize = 15;
+private async void AgregarRespuesta()
+{
+string respuesta = string.Empty;
+response = string.Empty;
 
-            string colorHexadecimal = "#a25523";
+string colorHexadecimal = "#a25523";
 
-            BrushConverter brushConverter = new BrushConverter();
-            Brush colorBrush = (Brush)brushConverter.ConvertFromString(colorHexadecimal);
-            TextBlock textBlock = new TextBlock();
-            textBlock.FontSize = fontSize;
-            textBlock.TextWrapping = TextWrapping.Wrap;
-            textBlock.Margin = margin_TxtBlk;
-            textBlock.Foreground = colorBrush;
-            StackPanel.Children.Add(textBlock);
-            textBlock.Text = "Gpt: ";
-            await chat.StreamResponseFromChatbotAsync(res =>
+BrushConverter brushConverter = new BrushConverter();
+Brush colorBrush = (Brush)brushConverter.ConvertFromString(colorHexadecimal);
+TextBlock textBlock = new TextBlock();
+textBlock.FontSize = fontSize;
+textBlock.TextWrapping = TextWrapping.Wrap;
+textBlock.Margin = margin_TxtBlk;
+textBlock.Foreground = colorBrush;
+StackPanel.Children.Add(textBlock);
+textBlock.Text = "Gpt: ";
+
+            try
             {
-                response = res;
-                textBlock.Text += response;
-                Scroll.LineDown();
-            });
-            role = response;
-            chat.AppendSystemMessage(role);
-        }
+                await chat.StreamResponseFromChatbotAsync(res =>
+                {
+                    response = res;
+                    textBlock.Text += response;
+                    Scroll.LineDown();
+                });
+                role = response;
+                chat.AppendSystemMessage(role);
 
-        private void AgregarPrompt(string _prompt)
-        {
-            TextBlock textBlock = new TextBlock();
-            textBlock.Margin = margin_TxtBlk;
-            textBlock.FontSize = fontSize;
-            textBlock.TextWrapping = TextWrapping.Wrap;
-            textBlock.Text = "Student: "+_prompt;
-            textBlock.Foreground = Brushes.White;
-            StackPanel.Children.Add(textBlock);
-        }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Se ha producido un error: {ex.Message}");
+            }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            Close();
-        }
+}
 
-        private void ColumnDefinition_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            DragMove();
-        }
+    private void AgregarPrompt(string _prompt)
+    {
+    TextBlock textBlock = new TextBlock();
+    textBlock.Margin = margin_TxtBlk;
+    textBlock.FontSize = fontSize;
+    textBlock.TextWrapping = TextWrapping.Wrap;
+    textBlock.Text = "Student: "+_prompt;
+    textBlock.Foreground = Brushes.White;
+    StackPanel.Children.Add(textBlock);
+    }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+    Close();
+    }
+
+    private void ColumnDefinition_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+        DragMove();
+    }
+
+    private void Button_Click_1(object sender, RoutedEventArgs e)
+    {
+    GenerarRespuesta();
+    }
+
+        private void TextBox_MouseLeave(object sender, MouseEventArgs e)
         {
-            GenerarRespuesta();
+            api = new OpenAIAPI(ApiTextBox.Text);
+            chat = api.Chat.CreateConversation();
         }
     }
-}
+    }
